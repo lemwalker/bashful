79cea7798ea71827a8959ae1be9b54fccd320fac
Lem Walker <walker.lem@gmail.com>
2019-07-07 17:33:53 -0500

    additions to bash_aliases

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -1,9 +1,21 @@
 # Bash customizations
+function is_defined() {
+    if which "$1" > /dev/null 2>&1; then
+        return 0
+    fi
+    if alias "$1" > /dev/null 2>&1; then
+        return 0
+    fi
+    return 1
+}
 
-# use 256 colors
-if which tput > /dev/null 2>&1 &&  [[ $(tput -T$TERM colors) -ge 8 ]]; then
-    export TERM=xterm-256color
+## use 256 colors
+if is_defined tput ; then
+    [[ $(tput -T$TERM colors) -ge 8 ]] && export TERM=xterm-256color
 fi
 
-alias topc='top -cu $USER'
+# set aliases only if they do not match existing aliases or tools
+! is_defined topc && alias topc='top -cu $USER'
+! is_defined ll   && alias ll='ls -al'
+! is_defined ltr  && alias ltr='ls -ltr'
 
